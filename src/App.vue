7ec8485743409f93360d43783be8a53877b6ec89
cm_fighting <cmtianxie@163.com>
2022-08-11 00:21:28 +0800

    页面完结

--- src/App.vue
+++ src/App.vue
@@ -1,108 +1,118 @@
 <script setup>
-import { getCurrentInstance, ref, toRefs } from 'vue';
-import { Search } from '@element-plus/icons-vue'
+import { getCurrentInstance, ref, toRefs } from "vue";
+import { Search } from "@element-plus/icons-vue";
 
 // 模糊搜索数据
-let queryName = ref('')
+let queryName = ref("");
 // 勾选项
-let multipleSelection = ref([])
+let multipleSelection = ref([]);
 // 对话框标题
-let dialogTitle = ref('添加')
+let dialogType = ref(1);
 // 对话框状态
-let dialogFormVisible = ref(false)
-const multipleTableRef = ref([])
+let dialogFormVisible = ref(false);
+const multipleTableRef = ref([]);
 
-const instance = getCurrentInstance()
+const instance = getCurrentInstance();
 
 const form = ref({
-  nickName: '',
-  eamil: '',
-  phone: '',
-  address: ''
-})
+  nickName: "",
+  eamil: "",
+  phone: "",
+  address: "",
+});
 
 const tableData = ref([
   {
     id: 1,
-    nickName: 'Tom',
-    email: 'tom@163.com',
-    phone: '18147474545',
-    address: 'No. 189, Grove St, Los Angeles'
+    nickName: "Tom",
+    email: "tom@163.com",
+    phone: "18147474545",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
     id: 2,
-    nickName: 'Jerry',
-    email: 'jerry@qq.com',
-    phone: '15123235656',
-    address: 'No. 189, Grove St, Los Angeles'
+    nickName: "Jerry",
+    email: "jerry@qq.com",
+    phone: "15123235656",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
     id: 3,
-    nickName: 'Lucy',
-    email: 'lucy@gmail.com',
-    phone: '13589897474',
-    address: 'No. 189, Grove St, Los Angeles'
+    nickName: "Lucy",
+    email: "lucy@gmail.com",
+    phone: "13589897474",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    nickName: 'Jet',
-    email: 'imjet@gmail.com',
-    phone: '13145458989',
-    address: 'No. 189, Grove St, Los Angeles'
+    nickName: "Jet",
+    email: "imjet@gmail.com",
+    phone: "13145458989",
+    address: "No. 189, Grove St, Los Angeles",
   },
   {
-    nickName: 'Kobe',
-    email: 'kobe@126.com',
-    phone: '13356569898',
-    address: 'No. 189, Grove St, Los Angeles'
+    nickName: "Kobe",
+    email: "kobe@126.com",
+    phone: "13356569898",
+    address: "No. 189, Grove St, Los Angeles",
   },
-])
+]);
 
 //勾选
 const handleSelectionChange = (val) => {
-  console.log(val)
-  multipleSelection.value = val
-}
+  console.log(val);
+  multipleSelection.value = val;
+};
 
 // 添加、编辑 打开对话框
 const openForm = (type) => {
-  console.log('open form', type)
+  console.log("open form", type);
+  dialogType.value = type;
   if (type === 1) {
-    console.log('add')
-    form.value = {}
+    console.log("add...");
+    form.value = {};
   }
   if (type === 2) {
-    dialogTitle.value = '编辑'
+    console.log("edit...");
   }
-  dialogFormVisible.value = true
-}
+  dialogFormVisible.value = true;
+};
 
 // 对话框确认-添加用户数据
-const addUser = () => {
-  dialogFormVisible.value = false
-  console.log('调用接口-添加数据', form.value)
-}
+const saveUser = () => {
+  dialogFormVisible.value = false;
+  if (dialogType.value == 1) {
+    console.log("add ...");
+  } else {
+    console.log("edit ...");
+  }
+};
 
 // 删除
 const handleRemove = (id) => {
-  console.log('删除用户', id)
-  let ids = [id]
-  console.log(ids)
-  cancelSelection()
-}
+  console.log("删除用户", id);
+  let ids = [id];
+  console.log(ids);
+  cancelSelection();
+};
 
 // 删除勾选列表
 const removeUserList = () => {
-  console.log(multipleSelection.value)
-  let ids = multipleSelection.value.map(item => item.id)
-  console.log(ids)
-  cancelSelection()
-}
+  console.log(multipleSelection.value);
+  let ids = multipleSelection.value.map((item) => item.id);
+  console.log(ids);
+  cancelSelection();
+};
 
 // 取消勾选
 const cancelSelection = () => {
-  var ultipleTabInstance = toRefs(instance.refs.multipleTableRef)
-  ultipleTabInstance.clearSelection.value()
-}
+  var ultipleTabInstance = toRefs(instance.refs.multipleTableRef);
+  ultipleTabInstance.clearSelection.value();
+};
+
+// 姓名搜索（模糊）
+const handlerQueryName = (val) => {
+  console.log('query by name', val)
+};
 
 </script>
 
@@ -114,7 +124,8 @@ const cancelSelection = () => {
     </div>
     <!-- 功能区域 -->
     <div class="fn">
-      <el-input v-model="queryName" style="width:35%" placeholder="Please input name ..." :prefix-icon="Search" />
+      <el-input v-model="queryName" style="width: 35%" placeholder="Please input name ..." :prefix-icon="Search"
+        @input="handlerQueryName" />
       <div class="fn-btn">
         <el-button type="primary" @click="openForm(1)">添加</el-button>
         <el-button v-if="multipleSelection.length > 0" type="danger" @click="removeUserList">删除</el-button>
@@ -123,13 +134,13 @@ const cancelSelection = () => {
     <!-- 表格 -->
     <div class="table">
       <el-table ref="multipleTableRef" @selection-change="handleSelectionChange" :data="tableData" style="width: 100%"
-        height="250" border>
+        :row-style="{ height: '80px' }" border>
         <el-table-column type="selection" width="55" />
         <el-table-column fixed prop="nickName" label="昵称" width="150" />
-        <el-table-column prop="email" label="邮箱" width="120" />
+        <el-table-column prop="email" label="邮箱" width="150" />
         <el-table-column prop="phone" label="联系电话" width="120" />
         <el-table-column prop="address" label="地址" width="600" />
-        <el-table-column fixed="right" label="操作" width="120">
+        <el-table-column fixed="right" label="操作" width="200">
           <template #default="scope">
             <el-button type="primary" link size="small" @click="openForm(2)">编辑</el-button>
             <el-button type="danger" link size="small" @click="handleRemove(scope.row.id)">删除</el-button>
@@ -138,7 +149,7 @@ const cancelSelection = () => {
       </el-table>
 
       <!-- 添加编辑的form对话框 -->
-      <el-dialog v-model="dialogFormVisible" :title="dialogTitle">
+      <el-dialog v-model="dialogFormVisible" :title="dialogType == 1 ? '添加' : '编辑'">
         <el-form :model="form">
           <el-form-item label="昵称" :label-width="100">
             <el-input v-model="form.nickName" autocomplete="off" />
@@ -162,20 +173,17 @@ const cancelSelection = () => {
         <template #footer>
           <span class="dialog-footer">
             <el-button @click="dialogFormVisible = false">取消</el-button>
-            <el-button type="primary" @click="addUser">确认</el-button>
+            <el-button type="primary" @click="saveUser()">确认</el-button>
           </span>
         </template>
       </el-dialog>
-
     </div>
   </div>
-
-
 </template>
 
 <style scoped>
 .table_wrap {
-  width: 800px;
+  width: 1000px;
   /* position: absolute;
   top: 50%;
   left: 50%;
